bfbe45510f890522ffd0ff8748e7dde9ea9ca4c9
samarjeet-singh-kheda <samarjeetsingh259@gmail.com>
2024-11-21 20:33:34 +0530

    Added loading state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,37 +2,41 @@ import { useState } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [jsonInput, setJsonInput] = useState("");
-  const [responseData, setResponseData] = useState(null);
+  const [input, setInput] = useState("");
+  const [response, setResponse] = useState(null);
   const [error, setError] = useState("");
-  const [filteredData, setFilteredData] = useState(null);
+  const [filteredResponse, setFilteredResponse] = useState(null);
   const [selectedFilters, setSelectedFilters] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async () => {
     try {
+      setLoading(true);
       setError("");
-      setFilteredData(null);
+      setFilteredResponse(null);
 
-      const parsedInput = JSON.parse(jsonInput);
+      const parsedInput = JSON.parse(input);
 
       const response = await axios.post(
         "https://bajaj-finserv-assignment-ufb5.onrender.com/bfhl",
         parsedInput
       );
-      setResponseData(response.data);
+      setResponse(response.data);
     } catch (err) {
       console.error(err);
 
       setError("Invalid JSON or API error.");
+    } finally {
+      setLoading(false);
     }
   };
 
   const handleInputChange = (e) => {
     const value = e.target.value;
-    setJsonInput(value);
+    setInput(value);
 
-    setResponseData(null);
-    setFilteredData(null);
+    setResponse(null);
+    setFilteredResponse(null);
     setSelectedFilters([]);
   };
 
@@ -43,54 +47,68 @@ const App = () => {
   };
 
   const handleClear = () => {
-    setJsonInput("");
-    setResponseData(null);
-    setFilteredData(null);
+    setInput("");
+    setResponse(null);
+    setFilteredResponse(null);
     setSelectedFilters([]);
+
+    setLoading(false);
   };
 
   const handleFilterChange = (selectedOptions) => {
     const filtered = selectedOptions.reduce((acc, filter) => {
-      acc[filter] = responseData[filter];
+      acc[filter] = response[filter];
       return acc;
     }, {});
-    setFilteredData(filtered);
+
+    setFilteredResponse(filtered);
     setSelectedFilters(selectedOptions);
   };
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
       <h1 className="text-3xl font-bold mb-6 text-center">API data</h1>
+
       <div className="w-full max-w-2xl">
         <textarea
           className="w-full p-4 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 resize-none mb-4"
           rows="5"
           placeholder='Enter JSON here. e.g. -> { "data": ["A", "C", "z"] }. 
-NOTE:- Use double inverted ("") commas for accurate results.'
-          value={jsonInput}
+NOTE:- Use double inverted (" ") commas for accurate results.'
+          value={input}
           onChange={handleInputChange}
           onKeyDown={handleKeyPress}
         />
+
         <div className="flex gap-2">
           <button
-            className="bg-blue-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700 active:bg-blue-800 transition-all flex-1"
+            className="bg-blue-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700 active:bg-blue-800 flex-1"
             onClick={handleSubmit}
           >
             Submit
           </button>
+
           <button
-            className="bg-gray-300 text-black font-bold py-2 px-4 rounded-lg hover:bg-gray-400 active:bg-gray-500 transition-all"
+            className="bg-gray-300 text-black font-bold py-2 px-4 rounded-lg hover:bg-gray-400 active:bg-gray-500"
             onClick={handleClear}
           >
             Clear
           </button>
         </div>
+
         {error && <p className="text-red-500 mt-4">{error}</p>}
       </div>
 
-      {responseData && (
+      {loading && (
+        <div className="text-center mt-8">
+          <div className="w-8 h-8 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+        </div>
+      )}
+
+      {response && !loading && (
         <div className="mt-6 w-full max-w-2xl bg-white p-4 rounded-lg shadow-md sticky top-4 z-10">
           <h2 className="text-xl font-bold mb-4">Filters</h2>
+
           <div className="flex flex-wrap gap-2">
             {["alphabets", "numbers", "highest_lowercase_alphabet"].map(
               (filter) => (
@@ -108,6 +126,7 @@ NOTE:- Use double inverted ("") commas for accurate results.'
                     }
                     className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded cursor-pointer"
                   />
+
                   {filter[0].toUpperCase() + filter.slice(1)}
                 </label>
               )
@@ -116,11 +135,11 @@ NOTE:- Use double inverted ("") commas for accurate results.'
         </div>
       )}
 
-      {filteredData && (
+      {filteredResponse && (
         <div className="mt-6 w-full max-w-2xl bg-white p-4 rounded-lg shadow-md">
           <h2 className="text-xl font-bold mb-4">Filtered Data</h2>
           <div className="bg-gray-100 p-4 rounded-lg">
-            {JSON.stringify(filteredData)}
+            {JSON.stringify(filteredResponse)}
           </div>
         </div>
       )}
